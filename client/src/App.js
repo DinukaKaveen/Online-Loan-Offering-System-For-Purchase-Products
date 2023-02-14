10a4fe10e9eaa626a72c01edbf13e17ffa96d652
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-02-14 18:09:37 +0530

    route

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import NavbarAdmin from "./components/admin/NavbarAdmin";
+import HomeAdmin from "./components/admin/HomeAdmin";
 import NavbarCus from "./components/customer/NavbarCus";
 
 export default function App() {
   return (
     <BrowserRouter>
-    <div>
-      <NavbarCus/>
-      <Routes>
-        <Route path="/admin" element={<NavbarAdmin/>}></Route>
-      </Routes>
-    </div>
+      <div>
+        <NavbarCus />
+        <Routes>
+          <Route path="/admin" element={<HomeAdmin />}></Route>
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
--- client/src/components/admin/HomeAdmin.js
+++ client/src/components/admin/HomeAdmin.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import NavbarAdmin from "./NavbarAdmin";
 
 export default function HomeAdmin() {
   return (
     <div>
-        
+      <NavbarAdmin />
+      <div className="p-4 sm:ml-64">
+        dsgj
+      </div>
     </div>
-  )
+  );
 }
