2446951a001fbffce9684d9bb2fbf484f5187450
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-02-15 01:36:29 +0530

    Update RegisterCus.js

--- client/src/components/customer/RegisterCus.js
+++ client/src/components/customer/RegisterCus.js
@@ -48,7 +48,8 @@ export default function RegisterCus() {
         </nav>
         <h2 className="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl dark:text-gray-700">
           Customer Registration
-        </h2><br/>
+        </h2>
+        <br />
 
         <form>
           <div className="grid gap-6 mb-6 md:grid-cols-2">
@@ -94,6 +95,11 @@ export default function RegisterCus() {
                 id="age"
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="18"
+                onKeyUp={(event) => {
+                  if (!/[0-9]/.test(event.key)) {
+                    event.preventDefault();
+                  }
+                }}
                 required
               />
             </div>
