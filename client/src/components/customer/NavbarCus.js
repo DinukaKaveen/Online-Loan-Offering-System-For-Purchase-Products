75eeb6bcfc33c3638844058d1f028021928bd4cb
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-02-14 18:36:51 +0530

    Update NavbarCus.js

--- client/src/components/customer/NavbarCus.js
+++ client/src/components/customer/NavbarCus.js
@@ -11,20 +11,20 @@ export default function NavbarCus() {
               className="h-6 mr-3 sm:h-9"
               alt="Flowbite Logo"
             />
-            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
               Flowbite
             </span>
           </a>
           <div className="flex items-center md:order-2">
             <a
               href="/"
-              className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+              className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-md px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
             >
               Login
             </a>
             <a
               href="/"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-md px-4 py-2 md:px-5 md:py-2.5 mr-1 md:mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
             >
               Sign up
             </a>
@@ -55,7 +55,7 @@ export default function NavbarCus() {
             id="mega-menu-icons"
             className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
           >
-            <ul className="flex flex-col mt-4 text-sm font-medium md:flex-row md:space-x-8 md:mt-0">
+            <ul className="flex flex-col mt-4 text-md font-medium md:flex-row md:space-x-8 md:mt-0">
               <li>
                 <a
                   href="/"
