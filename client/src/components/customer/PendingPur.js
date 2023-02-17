b0ce6bea97f26889f082479af8e153abdc6a366a
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-02-17 19:29:14 +0530

    Update PendingPur.js

--- client/src/components/customer/PendingPur.js
+++ client/src/components/customer/PendingPur.js
@@ -45,26 +45,18 @@ export default function PendingPur() {
                 $599
               </td>
               <td className="px-6 py-4">
-                <a
-                  href="/"
-                  className="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800"
+                <button
+                  type="button"
+                  className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800"
                 >
-                  <i className="fa-solid fa-eye"></i>
-                </a>
-                &nbsp;
-                <a
-                  href="/"
-                  className="text-yellow-700 border border-yellow-700 hover:bg-yellow-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-yellow-500 dark:text-yellow-500 dark:hover:text-white dark:focus:ring-yellow-800"
+                  <i className="fa-solid fa-eye"></i>&nbsp;View
+                </button>
+                <button
+                  type="button"
+                  className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
                 >
-                  <i className="fa-solid fa-pen-to-square"></i>
-                </a>
-                &nbsp;
-                <a
-                  href="/"
-                  className="text-red-700 border border-red-700 hover:bg-red-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:focus:ring-red-800"
-                >
-                  <i className="fa-solid fa-trash"></i>
-                </a>
+                  <i className="fa-solid fa-rotate-left"></i>&nbsp;Return
+                </button>
               </td>
             </tr>
             <tr className="bg-white border-b dark:bg-gray-200 dark:border-gray-300 hover:bg-gray-50 dark:hover:bg-gray-100">
@@ -81,26 +73,18 @@ export default function PendingPur() {
                 $599
               </td>
               <td className="px-6 py-4">
-                <a
-                  href="/"
-                  className="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800"
+                <button
+                  type="button"
+                  className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800"
                 >
-                  <i className="fa-solid fa-eye"></i>
-                </a>
-                &nbsp;
-                <a
-                  href="/"
-                  className="text-yellow-700 border border-yellow-700 hover:bg-yellow-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-yellow-500 dark:text-yellow-500 dark:hover:text-white dark:focus:ring-yellow-800"
+                  <i className="fa-solid fa-eye"></i>&nbsp;View
+                </button>
+                <button
+                  type="button"
+                  className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
                 >
-                  <i className="fa-solid fa-pen-to-square"></i>
-                </a>
-                &nbsp;
-                <a
-                  href="/"
-                  className="text-red-700 border border-red-700 hover:bg-red-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:focus:ring-red-800"
-                >
-                  <i className="fa-solid fa-trash"></i>
-                </a>
+                  <i className="fa-solid fa-rotate-left"></i>&nbsp;Return
+                </button>
               </td>
             </tr>
             <tr className="bg-white border-b dark:bg-gray-200 dark:border-gray-300 hover:bg-gray-50 dark:hover:bg-gray-100">
@@ -117,26 +101,18 @@ export default function PendingPur() {
                 $599
               </td>
               <td className="px-6 py-4">
-                <a
-                  href="/"
-                  className="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800"
+                <button
+                  type="button"
+                  className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800"
                 >
-                  <i className="fa-solid fa-eye"></i>
-                </a>
-                &nbsp;
-                <a
-                  href="/"
-                  className="text-yellow-700 border border-yellow-700 hover:bg-yellow-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-yellow-500 dark:text-yellow-500 dark:hover:text-white dark:focus:ring-yellow-800"
+                  <i className="fa-solid fa-eye"></i>&nbsp;View
+                </button>
+                <button
+                  type="button"
+                  className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
                 >
-                  <i className="fa-solid fa-pen-to-square"></i>
-                </a>
-                &nbsp;
-                <a
-                  href="/"
-                  className="text-red-700 border border-red-700 hover:bg-red-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:focus:ring-red-800"
-                >
-                  <i className="fa-solid fa-trash"></i>
-                </a>
+                  <i className="fa-solid fa-rotate-left"></i>&nbsp;Return
+                </button>
               </td>
             </tr>
           </tbody>
