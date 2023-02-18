9fbb1c401e8b309350da03d76c50d1c5d8a7396e
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-02-19 01:06:56 +0530

    Update ViewProduct.js

--- client/src/components/customer/purchases/ViewProduct.js
+++ client/src/components/customer/purchases/ViewProduct.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ViewProduct() {
   return (
-    <div style={{ margin: "50px" }}>
+    <div style={{ margin: "30px" }}>
       <h2 className="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl dark:text-gray-700">
         Product Details
       </h2>
