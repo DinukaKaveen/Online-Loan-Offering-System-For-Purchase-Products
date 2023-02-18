c1abd6ef7b8669c0b282ae56fcee70755b905752
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-02-19 00:58:41 +0530

    Update ViewProduct.js

--- client/src/components/customer/purchases/ViewProduct.js
+++ client/src/components/customer/purchases/ViewProduct.js
@@ -5,16 +5,16 @@ export default function ViewProduct() {
     <div>
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg" style={{margin: '50px'}}>
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase dark:text-gray-100">
             <tr>
               <th
                 scope="col"
-                className="px-6 py-3 bg-gray-50 dark:bg-gray-800"
+                className="px-6 py-3 bg-gray-50 dark:bg-gray-500"
                 style={{ width: "40%" }}
               >
                 Product name
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-6 py-3 dark:bg-gray-500">
                 Color
               </th>
             </tr>
@@ -23,47 +23,47 @@ export default function ViewProduct() {
             <tr className="border-b border-gray-200 dark:border-gray-700">
               <th
                 scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-gray-700 dark:bg-gray-300"
               >
                 Apple MacBook Pro 17"
               </th>
-              <td className="px-6 py-4">Silver</td>
+              <td className="px-6 py-4 dark:text-gray-700 dark:bg-gray-100">Silver</td>
             </tr>
             <tr className="border-b border-gray-200 dark:border-gray-700">
               <th
                 scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-gray-700 dark:bg-gray-300"
               >
                 Microsoft Surface Pro
               </th>
-              <td className="px-6 py-4">White</td>
+              <td className="px-6 py-4 dark:text-gray-700 dark:bg-gray-100">White</td>
             </tr>
             <tr className="border-b border-gray-200 dark:border-gray-700">
               <th
                 scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-gray-700 dark:bg-gray-300"
               >
                 Magic Mouse 2
               </th>
-              <td className="px-6 py-4">Black</td>
+              <td className="px-6 py-4 dark:text-gray-700 dark:bg-gray-100">Black</td>
             </tr>
             <tr className="border-b border-gray-200 dark:border-gray-700">
               <th
                 scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-gray-700 dark:bg-gray-300"
               >
                 Google Pixel Phone
               </th>
-              <td className="px-6 py-4">Gray</td>
+              <td className="px-6 py-4 dark:text-gray-700 dark:bg-gray-100">Gray</td>
             </tr>
             <tr>
               <th
                 scope="row"
-                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-gray-700 dark:bg-gray-300"
               >
                 Apple Watch 5
               </th>
-              <td className="px-6 py-4">Red</td>
+              <td className="px-6 py-4 dark:text-gray-700 dark:bg-gray-100">Red</td>
             </tr>
           </tbody>
         </table>
