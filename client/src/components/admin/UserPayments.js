f02392a3978b28866990fd4928d248698f72a9b0
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-02-24 10:10:42 +0530

    bearcrub update

--- client/src/components/admin/UserPayments.js
+++ client/src/components/admin/UserPayments.js
@@ -8,7 +8,7 @@ export default function UserPayments() {
     <div>
       <div className="p-4 sm:ml-64">
         <div className="p-4 mt-14">
-          <nav className="flex mb-4" aria-label="Breadcrumb">
+        <nav className="flex mb-4" aria-label="Breadcrumb">
             <ol className="inline-flex items-center space-x-1 md:space-x-3">
               <li className="inline-flex items-center">
                 <Link
@@ -29,6 +29,7 @@ export default function UserPayments() {
               <li>
                 <div className="flex items-center">
                   <svg
+                    aria-hidden="true"
                     className="w-6 h-6 text-gray-400"
                     fill="currentColor"
                     viewBox="0 0 20 20"
@@ -42,7 +43,7 @@ export default function UserPayments() {
                   </svg>
                   <Link
                     to={"/admin/Users"}
-                    className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray"
+                    className="ml-1 text-sm font-medium text-gray-700 hover:text-gray-900 md:ml-2 dark:text-gray-400 dark:hover:text-gray"
                   >
                     Users
                   </Link>
@@ -57,12 +58,12 @@ export default function UserPayments() {
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      fill-rule="evenodd"
+                      fillRule="evenodd"
                       d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
                       clipRule="evenodd"
                     ></path>
                   </svg>
-                  <span className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray">
+                  <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">
                     Payments
                   </span>
                 </div>
--- client/src/components/admin/UserPurchases.js
+++ client/src/components/admin/UserPurchases.js
@@ -32,20 +32,21 @@ export default function UserPurchases() {
               <li>
                 <div className="flex items-center">
                   <svg
+                    aria-hidden="true"
                     className="w-6 h-6 text-gray-400"
                     fill="currentColor"
                     viewBox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      fillRule="evenodd"
+                      fill-rule="evenodd"
                       d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
                       clipRule="evenodd"
                     ></path>
                   </svg>
                   <Link
                     to={"/admin/Users"}
-                    className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray"
+                    className="ml-1 text-sm font-medium text-gray-700 hover:text-gray-900 md:ml-2 dark:text-gray-400 dark:hover:text-gray"
                   >
                     Users
                   </Link>
@@ -65,7 +66,7 @@ export default function UserPurchases() {
                       clipRule="evenodd"
                     ></path>
                   </svg>
-                  <span className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray">
+                  <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">
                     Purchases
                   </span>
                 </div>
