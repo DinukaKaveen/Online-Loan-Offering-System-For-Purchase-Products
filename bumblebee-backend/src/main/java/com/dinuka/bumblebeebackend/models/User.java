22b23deaf27aa4920ea5724dba079476658a6e5b
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-02-25 23:23:29 +0530

    Update User.java

--- bumblebee-backend/src/main/java/com/dinuka/bumblebeebackend/models/User.java
+++ bumblebee-backend/src/main/java/com/dinuka/bumblebeebackend/models/User.java
@@ -1,4 +1,19 @@
 package com.dinuka.bumblebeebackend.models;
 
+import jakarta.persistence.Entity;
+
+import java.util.Date;
+
+@Entity //automatically generate table
 public class User {
+
+    private Long user_id;
+    private String first_name;
+    private String last_name;
+    private Date date_of_birth;
+    private String phone_no;
+    private String nic;
+    private String email;
+    private String address;
+    private String password;
 }
