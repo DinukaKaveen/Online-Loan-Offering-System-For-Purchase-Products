6056dbeeb63e1d19bb433b6a976c9841d755e601
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-02-26 15:47:47 +0530

    Update User.java

--- bumblebee-backend/src/main/java/com/dinuka/bumblebeebackend/model/User.java
+++ bumblebee-backend/src/main/java/com/dinuka/bumblebeebackend/model/User.java
@@ -12,8 +12,80 @@ public class User {
     private String last_name;
     private Date date_of_birth;
     private String nic;
+    private String phone_no;
     private String email;
     private String address;
     private String password;
-    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public Date getDate_of_birth() {
+        return date_of_birth;
+    }
+
+    public void setDate_of_birth(Date date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getPhone_no() {
+        return phone_no;
+    }
+
+    public void setPhone_no(String phone_no) {
+        this.phone_no = phone_no;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
