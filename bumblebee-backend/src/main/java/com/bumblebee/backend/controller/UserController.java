e634b113cd72694809714b7d97d56bb2069110e1
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-02-27 15:14:28 +0530

    update user

--- bumblebee-backend/src/main/java/com/bumblebee/backend/controller/UserController.java
+++ bumblebee-backend/src/main/java/com/bumblebee/backend/controller/UserController.java
@@ -3,6 +3,7 @@ package com.bumblebee.backend.controller;
 import com.bumblebee.backend.model.User;
 import com.bumblebee.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,11 @@ public class UserController {
     @PostMapping ("/RegisterCustomer")
     User newUser(@RequestBody User newUser){
         return userRepository.save(newUser);
+    }
 
+    @GetMapping("/admin/Users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 
 }
