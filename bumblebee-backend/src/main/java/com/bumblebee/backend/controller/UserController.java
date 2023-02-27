efe3002a347bc77b28514af05e62e2f157a046e0
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-02-27 12:40:53 +0530

    Update UserController.java

--- bumblebee-backend/src/main/java/com/bumblebee/backend/controller/UserController.java
+++ bumblebee-backend/src/main/java/com/bumblebee/backend/controller/UserController.java
@@ -3,7 +3,10 @@ package com.bumblebee.backend.controller;
 import com.bumblebee.backend.model.User;
 import com.bumblebee.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
