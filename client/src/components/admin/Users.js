48bce8d687e162d029fa636cdeac522d61d6c8d3
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-03-01 22:32:02 +0530

    search input update

--- client/src/components/admin/Users.js
+++ client/src/components/admin/Users.js
@@ -5,7 +5,6 @@ import "../admin/Users.css";
 import DataTable from "react-data-table-component";
 
 export default function Users() {
-  
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
@@ -25,7 +24,7 @@ export default function Users() {
     },
     {
       name: "Name",
-      selector: (row) => row.first_name,
+      selector: (row) => row.first_name + " " + row.last_name,
       sortable: true,
     },
     {
@@ -60,9 +59,13 @@ export default function Users() {
   const filteredData = users.filter(
     (item) =>
       item.first_name.toLowerCase().includes(searchText.toLowerCase()) ||
+      item.last_name.toLowerCase().includes(searchText.toLowerCase()) ||
       item.email.toLowerCase().includes(searchText.toLowerCase()) ||
       item.payment_status.toLowerCase().includes(searchText.toLowerCase()) ||
-      item.id.toString().toLowerCase().includes(searchText.toString().toLowerCase())
+      item.id
+        .toString()
+        .toLowerCase()
+        .includes(searchText.toString().toLowerCase())
   );
 
   // const rows = users.map((user, index) => ({
@@ -137,12 +140,35 @@ export default function Users() {
             pagination
             subHeader
             subHeaderComponent={
-              <input
-                type="text"
-                placeholder="Search"
-                value={searchText}
-                onChange={handleSearch}
-              />
+              <div>
+                <div class="relative">
+                  <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg
+                      aria-hidden="true"
+                      class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                      ></path>
+                    </svg>
+                  </div>
+                  <input
+                    value={searchText}
+                    onChange={handleSearch}
+                    type="search"
+                    id="search"
+                    class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-50 dark:border-gray-300 dark:placeholder-gray-500 dark:text-gray-500 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Search"
+                  />
+                </div>
+              </div>
             }
           ></DataTable>
         </div>
