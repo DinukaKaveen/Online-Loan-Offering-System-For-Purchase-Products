20eca0b9709f2f06421f1911e6eeff1479000182
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-03-03 13:35:34 +0530

    Update NavbarAdmin.js

--- client/src/components/admin/NavbarAdmin.js
+++ client/src/components/admin/NavbarAdmin.js
@@ -237,8 +237,8 @@ export default function NavbarAdmin() {
               </a>
             </li>
             <li>
-              <Link
-                to={"/admin/Users"}
+              <a
+                href="/admin/Users"
                 className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
               >
                 <svg
@@ -258,7 +258,7 @@ export default function NavbarAdmin() {
                 <span className="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
                   3
                 </span>
-              </Link>
+              </a>
             </li>
             <li>
               <Link
