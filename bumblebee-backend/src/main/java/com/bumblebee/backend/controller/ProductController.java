50e544c2c54e723e78c374fe085094ef3dda4d24
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-03-05 13:32:26 +0530

    update product

--- bumblebee-backend/src/main/java/com/bumblebee/backend/controller/ProductController.java
+++ bumblebee-backend/src/main/java/com/bumblebee/backend/controller/ProductController.java
@@ -1,4 +1,22 @@
 package com.bumblebee.backend.controller;
 
+import com.bumblebee.backend.model.User;
+import com.bumblebee.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
 public class ProductController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("admin/Products")
+    User newProduct(@RequestBody User newProduct){
+        return userRepository.save(newProduct);
+    }
 }
