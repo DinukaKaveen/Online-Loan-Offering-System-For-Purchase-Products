de1441d1045b7144efa0f770f2db37f3c3cf942c
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-03-05 15:51:50 +0530

    Update NewProduct.js

--- client/src/components/admin/NewProduct.js
+++ client/src/components/admin/NewProduct.js
@@ -31,7 +31,7 @@ export default function NewProduct() {
   };
   const onSubmit = async (e) => {
     e.preventDefault();
-    await axios.post("http://localhost:8080/RegisterCustomer", product);
+    await axios.post("http://localhost:8080/admin/NewProduct", product);
   };
 
   return (
