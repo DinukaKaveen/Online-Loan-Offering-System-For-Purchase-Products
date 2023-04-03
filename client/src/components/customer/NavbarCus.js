b7ea689485c5b66d39d2dbd5a3e9611e75f26e30
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-04-03 18:36:23 +0530

    Update NavbarCus.js

--- client/src/components/customer/NavbarCus.js
+++ client/src/components/customer/NavbarCus.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function NavbarCus() {
-  
+
   return (
     <div>
       <nav className="bg-white border-gray-200 px-2 md:px-4 py-2.5 dark:bg-gray-900">
@@ -60,7 +60,7 @@ export default function NavbarCus() {
             <ul className="flex flex-col mt-4 text-md font-medium md:flex-row md:space-x-8 md:mt-0">
               <li>
                 <Link
-                  to={"/"}
+                  to={"/Home"}
                   className="block py-2 pl-3 pr-4 text-blue-600 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-blue-500 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
                   aria-current="page"
                 >
