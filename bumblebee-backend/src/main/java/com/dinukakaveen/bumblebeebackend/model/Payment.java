84e6536fce4f31b3701dbf344cac72fccffb6efa
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2023-04-04 12:11:26 +0530

    create payment.java

--- bumblebee-backend/src/main/java/com/dinukakaveen/bumblebeebackend/model/Payment.java
+++ bumblebee-backend/src/main/java/com/dinukakaveen/bumblebeebackend/model/Payment.java
@@ -3,6 +3,7 @@ package com.dinukakaveen.bumblebeebackend.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.math.BigDecimal;
 
 @Entity
 public class Payment {
@@ -13,5 +14,63 @@ public class Payment {
     private Integer purchase_id;
     private Integer product_id;
     private Integer user_id;
-    
+    private BigDecimal payment_amount;
+    private String payment_state;
+    private String remarks;
+
+    public Integer getPayment_id() {
+        return payment_id;
+    }
+
+    public void setPayment_id(Integer payment_id) {
+        this.payment_id = payment_id;
+    }
+
+    public Integer getPurchase_id() {
+        return purchase_id;
+    }
+
+    public void setPurchase_id(Integer purchase_id) {
+        this.purchase_id = purchase_id;
+    }
+
+    public Integer getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(Integer product_id) {
+        this.product_id = product_id;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public BigDecimal getPayment_amount() {
+        return payment_amount;
+    }
+
+    public void setPayment_amount(BigDecimal payment_amount) {
+        this.payment_amount = payment_amount;
+    }
+
+    public String getPayment_state() {
+        return payment_state;
+    }
+
+    public void setPayment_state(String payment_state) {
+        this.payment_state = payment_state;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
 }
