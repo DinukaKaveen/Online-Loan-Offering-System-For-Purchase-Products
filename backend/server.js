f3bcee2fd762999059241dee7c5c0415046f5258
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2024-02-04 21:14:03 +0530

    connect db

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.json());
@@ -11,3 +12,12 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 })
+
+mongoose
+    .connect(process.env.DB_URL)
+    .then((x) => {
+        console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    })
+    .catch((err) => {
+        console.error("Error connecting to mongo: ", err)
+    })
