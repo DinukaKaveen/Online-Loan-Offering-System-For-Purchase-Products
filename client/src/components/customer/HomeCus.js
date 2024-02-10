c084c21e6bc10a1a12ad3b2e679c9f41f28218e8
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2024-02-10 19:43:47 +0530

    retrieve products

--- client/src/components/customer/HomeCus.js
+++ client/src/components/customer/HomeCus.js
@@ -17,10 +17,13 @@ export default function HomeCus() {
 
   return (
     <div>
-      <ul>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
         {products.map((product, index) => (
-          <li key={index}>
-            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <div key={index}>
+            <div
+              className="w-full max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+              style={{ margin: "50px", width: "280px" }}
+            >
               <a href="#">
                 <img
                   className="p-8 rounded-t-lg"
@@ -87,21 +90,21 @@ export default function HomeCus() {
                   </span>
                 </div>
                 <div className="flex items-center justify-between">
-                  <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                  <span className="text-xl font-bold text-gray-900 dark:text-white">
                     {product.price}
                   </span>
                   <a
                     href="#"
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                   >
-                    Add to cart
+                    Buy Now
                   </a>
                 </div>
               </div>
             </div>
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
 
       <FooterCus />
     </div>
