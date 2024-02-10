3469573a179af2a4e6393904d68b0c0f342b9cbf
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2024-02-10 11:12:36 +0530

    Update NavbarAdmin.js

--- client/src/components/admin/NavbarAdmin.js
+++ client/src/components/admin/NavbarAdmin.js
@@ -191,7 +191,7 @@ export default function NavbarAdmin() {
               <ul id="dropdown-example" className="hidden py-2 space-y-2">
                 <li>
                   <a
-                    href="/admin/Products"
+                    href="/admin/products"
                     className="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                   >
                     Products
@@ -237,7 +237,7 @@ export default function NavbarAdmin() {
             </li>
             <li>
               <a
-                href="/admin/Users"
+                href="/admin/users"
                 className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
               >
                 <svg
