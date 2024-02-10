2c39f94ce4bdc615aa8a9889a69e46828771d618
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2024-02-10 21:07:53 +0530

    Update HomeCus.js

--- client/src/components/customer/HomeCus.js
+++ client/src/components/customer/HomeCus.js
@@ -173,7 +173,7 @@ export default function HomeCus() {
                     href="#"
                     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                   >
-                    Buy Now
+                    Add to List
                   </a>
                 </div>
               </div>
