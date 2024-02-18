e6529e548665dda6f89f3f973b2fd848680773a0
DinukaKaveen <94813111+DinukaKaveen@users.noreply.github.com>
2024-02-18 19:17:03 +0530

    create user login

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -7,10 +7,9 @@ const jwt = require("jsonwebtoken");
 
 router.post("/user_register", async (req, res) => {
   try {
-
     existingUser = await User.findOne({ email: req.body.email });
 
-    if(existingUser) {
+    if (existingUser) {
       return res.status(400).json({
         success: false,
         message: "User already exists",
@@ -35,19 +34,47 @@ router.post("/user_register", async (req, res) => {
         message: "Failed to create user",
       });
     }
-
   } catch (error) {
     console.log(error);
   }
-
 });
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 router.post("/user_login", async (req, res) => {
   try {
-    
-  }
-  catch (error) {
+    const findUser = await User.findOne({ email: req.body.email });
+
+    if(!findUser) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Email not found" });
+    }
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      findUser.password
+    );
+
+    if (validPassword) {
+      // create sign in token
+      const token = createToken(findUser._id);
+      res.cookie("access-tokekn", token);
+      return res.status(200).json({ success: true });
+
+    } else {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+
+  } catch (error) {
     console.log(error);
+    res.status(500).json({ success: false, message: "Server error" });
   }
 });
 
