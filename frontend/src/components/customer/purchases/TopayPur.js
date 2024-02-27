30c72dc5366fa622f6ebffcb969dc413899613de
Dinuka Kaveen Dominguhewa <dinukadominguhewa@gmail.com>
2024-02-27 09:29:02 +0530

    Update TopayPur.js

--- frontend/src/components/customer/purchases/TopayPur.js
+++ frontend/src/components/customer/purchases/TopayPur.js
@@ -59,7 +59,7 @@ export default function TopayPur() {
     <div>
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-200 dark:text-gray-400">
             <tr>
               <th scope="col" className="px-16 py-3">
                 <span className="sr-only">Image</span>
@@ -82,7 +82,7 @@ export default function TopayPur() {
             {products.map((product, index) => (
               <tr
                 key={index}
-                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                className="bg-white border-b dark:bg-gray-80 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-250"
               >
                 <td className="p-4">
                   <img
@@ -91,13 +91,13 @@ export default function TopayPur() {
                     alt="product_image"
                   />
                 </td>
-                <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                <td className="px-6 py-4 font-semibold text-gray-900 dark:text-gray-900">
                   {product.product_name}
                 </td>
                 <td className="px-6 py-4">
                   <div className="flex items-center">
                     <button
-                      className="inline-flex items-center justify-center p-1 me-3 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                      className="inline-flex items-center justify-center p-1 me-3 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-100 dark:text-gray-400 dark:border-gray-400 dark:hover:bg-gray-200 dark:hover:border-gray-400 dark:focus:ring-gray-700"
                       type="button"
                     >
                       <span className="sr-only">Quantity button</span>
@@ -121,13 +121,13 @@ export default function TopayPur() {
                       <input
                         type="number"
                         id="first_product"
-                        className="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        className="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-80 dark:border-gray-300 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         value={cartItems[index].quantity}
                         onChange={(e) => {}}
                       />
                     </div>
                     <button
-                      className="inline-flex items-center justify-center h-6 w-6 p-1 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                      className="inline-flex items-center justify-center h-6 w-6 p-1 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-100 dark:text-gray-400 dark:border-gray-400 dark:hover:bg-gray-200 dark:hover:border-gray-400 dark:focus:ring-gray-700"
                       type="button"
                     >
                       <span className="sr-only">Quantity button</span>
@@ -149,7 +149,7 @@ export default function TopayPur() {
                     </button>
                   </div>
                 </td>
-                <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                <td className="px-6 py-4 font-semibold text-gray-900 dark:text-gray-900">
                   {product.price.toFixed(2)}
                 </td>
                 <td className="px-6 py-4">
@@ -164,7 +164,7 @@ export default function TopayPur() {
             ))}
           </tbody>
           <tfoot>
-            <tr className="text-lg text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-white">
+            <tr className="text-lg text-gray-700 uppercase bg-gray-50 dark:bg-gray-500 dark:text-white">
               <th scope="row" className="px-6 py-3 text-base">
                 Total
               </th>
@@ -219,7 +219,6 @@ export default function TopayPur() {
                       </div>
 
                       <Pay />
-
                     </div>
                   </div>
                 </div>
