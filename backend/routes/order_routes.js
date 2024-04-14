b5220634b8e9e2eca6fd5506888b6d693d1e6283
Dinuka Kaveen Dominguhewa <dinukadominguhewa@gmail.com>
2024-04-14 21:59:21 +0530

    Update order_routes.js

--- backend/routes/order_routes.js
+++ backend/routes/order_routes.js
@@ -3,21 +3,26 @@ const router = express.Router();
 const Order = require("../models/Order");
 
 router.post("/create_order", async (req, res) => {
-  const order = new Order({
-    products: req.body.products,
-    user_id: req.body.user_id,
-    total_price: req.body.total_price,
-    paid_amount: req.body.paid_amount,
-  });
-
-  order
-    .save()
-    .then(() => {
-      return res.status(200).json({ success: true, message: "Order Created" });
-    })
-    .catch((error) => {
-      return res.status(500).json({ success: false, message: "Order Fail" });
+  try {
+    const order = new Order({
+      products: req.body.products,
+      user_id: req.body.user_id,
+      total_price: req.body.total_price,
+      paid_amount: req.body.paid_amount,
     });
+
+    const newOrder = await order.save();
+    if (newOrder) {
+      return res.status(200).json({ success: true, message: "Order Created" });
+    } else {
+      return res.status(400).json({ success: false, message: "Order Fail" });
+    }
+    
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
 });
 
 module.exports = router;
