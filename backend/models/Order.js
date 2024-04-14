829f23fa019a7e4f6abd1891e970684420699b06
Dinuka Kaveen Dominguhewa <dinukadominguhewa@gmail.com>
2024-04-14 14:42:07 +0530

    order model

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,23 +1,36 @@
 const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
-    user_id: {
-        type: String,
+  products: [
+    {
+      product_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
         required: true,
-    },
-    product_id: {
-        type: String,
+      },
+      quantity: {
+        type: Number,
         required: true,
+      },
     },
-    date_time: {
-        type: Date,
-        required: true,
-    },
-    order_status: {
-        default: "Unpaid",
-        type: String,
-        required: true,
-    },
+  ],
+  user_id: {
+    type: String,
+    required: true,
+  },
+  total_price: {
+    type: String,
+    required: true,
+  },
+  date_time: {
+    type: Date,
+    required: true,
+  },
+  status: {
+    default: "Installment",
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model("purchases", orderSchema);
+module.exports = mongoose.model("Order", orderSchema);
