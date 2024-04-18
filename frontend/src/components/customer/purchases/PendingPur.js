1cee150950789c0d0c4761acecf630b7fe72e402
Dinuka Kaveen Dominguhewa <dinukadominguhewa@gmail.com>
2024-04-18 21:06:54 +0530

    Update PendingPur.js

--- frontend/src/components/customer/purchases/PendingPur.js
+++ frontend/src/components/customer/purchases/PendingPur.js
@@ -1,81 +1,73 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import DataTable from "react-data-table-component";
 
 export default function PendingPur() {
-  const logging_id = sessionStorage.getItem("user_id");
   const [PendingProducts, setPendingProducts] = useState([]);
 
   useEffect(() => {
     //loadPendingProducts();
   }, []);
 
-  const loadPendingProducts = async () => {
-    const result = await axios.get(`http://localhost:8080/Purchase/${logging_id}/Pending`);
-    setPendingProducts(result.data);
-  };
-
-  const columns = [
-    {
-      name: "Purchase ID",
-      selector: (row) => row.purchase_id,
-      sortable: true,
-    },
-    {
-      name: "Product ID",
-      selector: (row) => row.product_id,
-      sortable: true,
-    },
-    {
-      name: "Qty",
-      selector: (row) => row.qty,
-      sortable: true,
-    },
-    {
-      name: "Price",
-      selector: (row) => row.purchase_amount,
-      sortable: true,
-    },
-    {
-      name: "Action",
-      width: "322px",
-      selector: (row) => (
-        <div>
-          <button
-            type="button"
-            className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800"
-          >
-            <i className="fa-solid fa-eye"></i>&nbsp;View
-          </button>
-          <button
-            type="button"
-            className="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
-          >
-            <i className="fa-solid fa-rotate-left"></i>&nbsp;Return
-          </button>
-          <a
-            href="/Payments"
-            className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-          >
-            Pay&nbsp;&nbsp;
-            <i className="fa-sharp fa-solid fa-angle-right"></i>
-          </a>
-        </div>
-      ),
-    },
-  ];
-
   return (
     <div>
-      <DataTable
-        columns={columns}
-        data={PendingProducts}
-        //fixedHeader
-        responsive
-        highlightOnHover
-        pagination
-        subHeader
-      ></DataTable>
+      <div id="accordion-open" data-accordion="open">
+        <h2 id="accordion-open-heading-3">
+          <button
+            type="button"
+            className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+            data-accordion-target="#accordion-open-body-3"
+            aria-expanded="false"
+            aria-controls="accordion-open-body-3"
+          >
+            <span className="flex items-center">
+              What are the differences between Flowbite and Tailwind UI?
+            </span>
+            <svg
+              data-accordion-icon
+              className="w-3 h-3 rotate-180 shrink-0"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 10 6"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M9 5 5 1 1 5"
+              />
+            </svg>
+          </button>
+        </h2>
+        <div
+          id="accordion-open-body-3"
+          className="hidden"
+          aria-labelledby="accordion-open-heading-3"
+        >
+          <div className="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
+            <div
+              className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-300 dark:bg-gray-100"
+            >
+              <img
+                className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                src="/docs/images/blog/image-4.jpg"
+                alt=""
+              />
+              <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-gray-700">
+                  Noteworthy technology acquisitions 2021
+                </h5>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-600">
+                  Here are the biggest enterprise technology acquisitions of
+                  2021 so far, in reverse chronological order.
+                </p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
