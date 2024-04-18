16570496d426b1d4763ff35a16970f11fe008790
Dinuka Kaveen Dominguhewa <dinukadominguhewa@gmail.com>
2024-04-18 21:48:53 +0530

    Update PendingPur.js

--- frontend/src/components/customer/purchases/PendingPur.js
+++ frontend/src/components/customer/purchases/PendingPur.js
@@ -20,7 +20,13 @@ export default function PendingPur() {
             aria-controls="accordion-open-body-3"
           >
             <span className="flex items-center">
-              What are the differences between Flowbite and Tailwind UI?
+              Order ID: | Date: | Price: | Paid Amount: | Pending Amount: &nbsp;
+              <button
+                type="button"
+                class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+              >
+                Make Payment
+              </button>
             </span>
             <svg
               data-accordion-icon
@@ -46,9 +52,7 @@ export default function PendingPur() {
           aria-labelledby="accordion-open-heading-3"
         >
           <div className="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
-            <div
-              className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-300 dark:bg-gray-100"
-            >
+            <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-300 dark:bg-gray-100">
               <img
                 className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
                 src="/docs/images/blog/image-4.jpg"
@@ -56,16 +60,20 @@ export default function PendingPur() {
               />
               <div className="flex flex-col justify-between p-4 leading-normal">
                 <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-gray-700">
-                  Noteworthy technology acquisitions 2021
+                  Product Name
                 </h5>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-600">
-                  Here are the biggest enterprise technology acquisitions of
-                  2021 so far, in reverse chronological order.
+                  Price:
+                </p>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-600">
+                  Qty:
+                </p>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-600">
+                  Warranty:
                 </p>
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </div>
