85b8b59ef46ea48ae5658652d53049a66ad9c7b0
Dinuka Kaveen Dominguhewa <dinukadominguhewa@gmail.com>
2024-04-18 14:11:32 +0530

    Update order_routes.js

--- backend/routes/order_routes.js
+++ backend/routes/order_routes.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const Order = require("../models/Order");
+const User = require("../models/User");
 
 router.post("/create_order", async (req, res) => {
   try {
+
+    const { products, user, total_price, paid_amount, installment_state, remarks, status } = req.body;
+
     const order = new Order({
-      products: req.body.products,
-      user_id: req.body.user_id,
-      total_price: req.body.total_price,
-      paid_amount: req.body.paid_amount,
-      installment_state: req.body.installment_state,
-      remarks: req.body.remarks,
-      status: req.body.status
+      products: products,
+      user_id: user._id,
+      total_price: total_price,
+      paid_amount: paid_amount,
+      installment_state: installment_state,
+      remarks: remarks,
+      status: status,
     });
 
     const newOrder = await order.save();
     if (newOrder) {
+
+      await User.findOneAndUpdate(user._id, { paid_amount: user.paid_amount + paid_amount });
+
       return res.status(200).json({ success: true, message: "Order Created Successfully" });
     } else {
       return res.status(400).json({ success: false, message: "Order Fail" });
