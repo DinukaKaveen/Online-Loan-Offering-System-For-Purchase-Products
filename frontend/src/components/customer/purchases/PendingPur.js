dff6bed5e60130be574d804b52653bcae0cf465d
Dinuka Kaveen Dominguhewa <dinukadominguhewa@gmail.com>
2024-04-30 12:01:51 +0530

    Update PendingPur.js

--- frontend/src/components/customer/purchases/PendingPur.js
+++ frontend/src/components/customer/purchases/PendingPur.js
@@ -74,7 +74,7 @@ export default function PendingPur() {
                   <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-300 dark:bg-gray-100">
                     <img
                       className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-                      src="/docs/images/blog/image-4.jpg"
+                      src={`http://localhost:8000/uploads/${products.find((product) => product._id === item.product_id) ?. image || "N/A"}`}
                       alt=""
                     />
                     <div className="flex flex-col justify-between p-4 leading-normal">
