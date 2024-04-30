eb805e9f575d5b64276a03875d9e7884f6f2c86a
Dinuka Kaveen Dominguhewa <dinukadominguhewa@gmail.com>
2024-04-30 10:09:41 +0530

    Update PendingPur.js

--- frontend/src/components/customer/purchases/PendingPur.js
+++ frontend/src/components/customer/purchases/PendingPur.js
@@ -19,7 +19,8 @@ export default function PendingPur() {
     const productResult = await axios.get("http://localhost:8000/products");
 
     setPendingOrders(orders.data.pendingOrders);
-    console.log(orders.data.pendingOrders[0].products)
+
+    console.log(orders.data.pendingOrders[0].products);
 
     const pendingOrders = orders.data.pendingOrders;
     const products = productResult.data.products;
@@ -28,7 +29,7 @@ export default function PendingPur() {
   }
 
   return (
-    <div>    
+    <div>
       <div id="accordion-open" data-accordion="open">
         {pendingOrders.map((order, index) => (
           <div key={index}>
@@ -41,9 +42,14 @@ export default function PendingPur() {
                 aria-controls={`accordion-open-body-${index}`}
               >
                 <span className="flex items-center">
-                  Order ID: {order._id} | Date: {order.date_time} | Price:  | Paid Amount: {order.paid_amount} | Pending Amount: {order.total_price - order.paid_amount}
-                  &nbsp;
+                  Order ID: {order._id} | Date: {order.date_time} | Price: | Paid Amount: {order.paid_amount} | Pending Amount: {order.total_price - order.paid_amount}
                 </span>
+                <button
+                  type="button"
+                  className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+                >
+                  Make Payment
+                </button>
                 <svg
                   data-accordion-icon
                   className="w-3 h-3 rotate-180 shrink-0"
@@ -67,36 +73,31 @@ export default function PendingPur() {
               className="open"
               aria-labelledby={`accordion-open-heading-${index}`}
             >
-              <div className="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
-                <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-300 dark:bg-gray-100">
-                  <img
-                    className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
-                    src="/docs/images/blog/image-4.jpg"
-                    alt=""
-                  />
-                  <div className="flex flex-col justify-between p-4 leading-normal">
-                    <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-gray-700">
-                      Product Name
-                    </h5>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-600">
-                      Price:
-                    </p>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-600">
-                      Qty:
-                    </p>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-600">
-                      Warranty:
-                    </p>
+              {pendingOrders[index].products.map((item, index2) => (
+                <div key={index2} className="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
+                  <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-300 dark:bg-gray-100">
+                    <img
+                      className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                      src="/docs/images/blog/image-4.jpg"
+                      alt=""
+                    />
+                    <div className="flex flex-col justify-between p-4 leading-normal">
+                      <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-gray-700">
+                        {item.product_id}
+                      </h5>
+                      <p className="mb-3 font-normal text-gray-700 dark:text-gray-600">
+                        Price:
+                      </p>
+                      <p className="mb-3 font-normal text-gray-700 dark:text-gray-600">
+                        Qty:
+                      </p>
+                      <p className="mb-3 font-normal text-gray-700 dark:text-gray-600">
+                        Warranty:
+                      </p>
+                    </div>
                   </div>
                 </div>
-                <br></br>
-                <button
-                  type="button"
-                  className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
-                >
-                  Make Payment
-                </button>
-              </div>
+              ))}
             </div>
           </div>
         ))}
